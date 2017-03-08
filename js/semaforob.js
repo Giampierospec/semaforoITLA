58762e2e74173451d3d580aa7bd0e000e9a98193
Giampiero <giampi_12@hotmail.com>
2017-03-07 22:11:00 -0400

    Fixed some bugs

--- js/semaforob.js
+++ js/semaforob.js
@@ -1,9 +1,10 @@
-var color = "";
+
 var red = document.getElementById("Red");
 var yellow = document.getElementById("Yellow");
 var green = document.getElementById("Green");
 
 function ciclo(){
+  var color = "Red";
   $.ajax({
     url: 'server.php',
     method: 'get',
@@ -23,7 +24,7 @@ function ciclo(){
   }
   clearLights();
   $("#"+color).css("background-color",color.toLowerCase());
-  setTimeout(ciclo, 100);
+  setTimeout(ciclo, 500);
 }
 function clearLights(){
     red.style.backgroundColor = "#111";
